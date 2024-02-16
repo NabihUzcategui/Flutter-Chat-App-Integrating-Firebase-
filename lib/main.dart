da92ddd661d989b031d2d46027455c9467e8262e
Nabih Uzcategui <nabihuzcateguip@gmail.com>
2024-02-16 18:40:28 -0300

    actualización Firebase_Auth

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'services/login_or_register_page.dart';
+import 'package:flutter_chat_app/services/auth/auth_gate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginOrRegisterPage(),
+      home: const AuthGate(),
     );
   }
 }
